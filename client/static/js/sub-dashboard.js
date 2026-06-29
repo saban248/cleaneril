aeead7274bf3553c6c29ed965ab24b5adb6156a8
saban <sabann248@gmail.com>
2026-06-29 21:32:46 +0300

    12/20 - tax + receipts, 09.05.2026

--- client/static/js/sub-dashboard.js
+++ client/static/js/sub-dashboard.js
@@ -1,17 +1,45 @@
 
 // MANAGER DASHBOARD 
 
-function MDSetTtitle(){
-    const manager = getManagerById(c_runtime.currentManagerIdView);
-    const company = getCompanyByManagerId(c_runtime.currentManagerIdView);
-    if (!company)return
+function MDSetTtitle(m, c){
     const titleManager = document.getElementById("managerNameTitle");
-    titleManager.textContent = company.owner_fullname;
+    titleManager.textContent = c.owner_fullname;
+}
+function MDSetLogoCompany(m, c){
+    const logo = document.getElementById("companyLogo")
+    const logo_path = `/static/images/logo/${c.logo_path}`
+    logo.style.backgroundImage = `url('${logo_path}')`;
+    logo.style.backgroundSize = "cover";
+    logo.style.backgroundPosition = "center";
+    logo.style.backgroundRepeat = "no-repeat";
+}
+
+function MDSetViewSummary(m, c){
+    const fullname = document.getElementById("managerName");
+    const managerPhone = document.getElementById("managerPhone");
+    const managerUserName = document.getElementById("managerUserName");
+    const companyName = document.getElementById("companyName");
+    const companyVAT = document.getElementById("companyVAT");
+    const companyPhone = document.getElementById("companyPhone")
+    const companyEmail = document.getElementById("companyEmail");
+
+    fullname.textContent = c.owner_fullname;
+    managerPhone.textContent = m.phone;
+    managerUserName.textContent = m.username;
+    companyName.textContent = c.company_name;
+    companyVAT.textContent = Boolean(c.vat_company)?"מורשה":"פטור"
+    companyPhone.textContent = c.company_phone;
+    companyEmail.textContent = c.company_email;
 }
 
 
 function onLoadManagerDashboard(){
-    MDSetTtitle()
+    const manager = getManagerById(c_runtime.currentManagerIdView);
+    const company = getCompanyByManagerId(c_runtime.currentManagerIdView);
+    if (!company)return
+    MDSetTtitle(manager, company)
+    MDSetLogoCompany(manager, company)
+    MDSetViewSummary(manager, company)
     switchManagerTab(MDTabsView.SUMMARY)
 }
 
